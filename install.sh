6b417ed6ef7f128a77fcd323c5c6af1b832ddd78
Virus Net <107586172+virusnet1@users.noreply.github.com>
2022-08-07 08:00:40 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,59 @@
+a='\e[1;31m'
+b='\e[1;32m'
+c='\e[1;33m'
+d='\e[1;34m'
+e='\e[1;35m'
+f='\e[1;36m'
+g='\e[1;37m'
+clear
+sleep 0.5
+echo -e $a "//////////"
+sleep 3
+echo -e $a "///////////////////"
+sleep 3
+echo -e $a "////////////////////////////"
+sleep 3
+echo -e $a "//////////////////////////////////////"
+sleep 3
+echo -e $a "////////////////////////////////////////////"
+sleep 3
+echo -e $a "///////////////////////////////////////////////////"
+echo
+echo -e $b "DONE..."
+sleep 3
+echo -e $d "PLEASE WAIT"
+sleep 3
+clear
+echo -e $c '  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\  WHAT IS YOUR SYS  //////////////////////  '
+printf "\n"
+echo -e $f "                              [1] KALI LINUX                                                 "
+echo
+echo -e $f "                              [2]   TERMUX                                                 "
+echo
+echo -e $f "                              [3]    EXIT                                                 "
+read -p '[+]ENTER YOUT SYSTEM : ' SYSTEM
+if [ $SYSTEM = 1 ];
+then
+apt update && upgrade -y
+apt install python -y
+pip install youtube-dl
+clear
+chmod 777 tube.sh
+./tube.sh
+fi
+if [ $SYSTEM = 2 ];
+then
+pkg update -y
+apt upgrade -y
+pkg install python -y
+pip install youtube-dl -y
+clear
+chmod 777 tube.sh
+./tube.sh
+fi
+if [ $SYSTEM = 3 ];
+  exit
+  clear
+  else
+  bash install.sh
+fi
